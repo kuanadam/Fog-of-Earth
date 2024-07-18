51fc0349802b034586061720d3893379f95a1818
kuanadam <adam.kuan@outlook.com>
2024-07-17 23:37:52 -0400

    feat: connecting app to firebase

--- android/app/src/main/kotlin/com/example/fog_of_earth/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fog_of_earth/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.fog_of_earth
+package com.example.fogofearth
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'features/auth/presentation/bloc/splash_cubit.dart';
 import 'features/auth/presentation/pages/splash_page.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
