4a693ecd23aaa05fc15ef1b0d737b35f450b1dd3
adamk2002 <86808155+ernolfur@users.noreply.github.com>
2024-07-22 20:35:33 -0400

    feat: initializing home_page route

---
+++ lib/features/auth/presentation/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        // You can add other properties here like `currentIndex`, `onTap`, etc.
+      ),
+      body: Center(
+        child: Text('Home Page Content'),
+      ),
+    );
+  }
+}
