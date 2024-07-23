c8f8f41fe83a9b05ed6196469f45513acae3a422
adamk2002 <86808155+ernolfur@users.noreply.github.com>
2024-07-23 19:46:59 -0400

    feat: integrated Google Maps API into homepage

--- lib/features/auth/presentation/pages/home_page.dart
+++ lib/features/auth/presentation/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fog_of_earth/features/auth/presentation/pages/auth/auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +19,45 @@ class HomePage extends StatelessWidget {
             icon: Icon(Icons.person),
             label: 'Profile',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_back_ios_new_outlined),
+            label: 'Back',
+          ),
         ],
         // You can add other properties here like `currentIndex`, `onTap`, etc.
       ),
-      body: Center(
-        child: Text('Home Page Content'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 720, // Set a fixed height for the GoogleMap
+              child: GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: _pGooglePlex,
+                  zoom: 13,
+                ),
+              ),
+            ),
+            Center(
+              child: Text('Home Page Content'),
+            ),
+            _signout(context),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget _signout(BuildContext context) {
+  return ElevatedButton(
+    onPressed: () async {
+      await Auth().SignOut(
+        context: context,
+      );
+    },
+    child: Text(
+      'Sign out',
+    ),
+  );
 }
